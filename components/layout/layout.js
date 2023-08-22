fbf1830f371829f81b48c1c0533a41e5cc12d91f
ahmaderaqi <ahmadzaher573@gmail.com>
2023-08-22 18:11:11 +0000

    feat: v1 of website

---
+++ components/layout/layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="bg-gray-100">
+      <Header/>
+      <div className="pt-10">
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
